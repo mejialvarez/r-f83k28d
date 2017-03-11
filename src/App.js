67dea66cd0a94f25e77c982c320a7ebaf61a9809
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2017-03-11 15:09:01 -0500

    Adds incrementCounter method

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementCounter() {
+    this.setState({
+      counter: ++this.state.counter
+    });
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.incrementCounter.bind(this)}>Incrementa</button>
       </div>
     );
   }
